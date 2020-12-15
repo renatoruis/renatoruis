282ef88f223e18698884560dde743aa397d70311
Renato Ruis <renatoruis@MacBook-Pro-de-Renato.local>
2020-12-15 12:58:03 -0300

    remove footer home

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-          Powered by Renatão
+          {/* Powered by Renatão */}
       </footer>
     </div>
   )
